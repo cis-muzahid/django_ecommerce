9d83feefc3dc502af85d6fd16cc8b0ab0ae3a896
cis-pragya <pragya.s@cisinlabs.com>
2024-03-06 20:00:33 +0530

    Authenticated Admin and Supplier login on Admin panel Resolved updation issues for users, roles and permissions.

--- users/models.py
+++ users/models.py
@@ -5,6 +5,25 @@ from django.utils.translation import gettext_lazy as _
 # class CustomUserManager(BaseUserManager):
 
 
+class UserManager(BaseUserManager):
+    def create_user(self, email, password, **extra_fields):
+
+        if not email:
+            raise ValueError('The Email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_active', True)
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        return self.create_user(email, password, **extra_fields)
+
 
 class Permission(models.Model):
 	name = models.CharField(max_length=255)
@@ -28,10 +47,10 @@ class CustomUser(AbstractUser):
 	# user_role = models.CharField(max_length=20, choices=ROLE_CHOICES, default=customer)
 	user_role = models.ForeignKey(Role, on_delete=models.CASCADE, blank=True, null=True)
 
-	# objects = CustomUserManager()
+	objects = UserManager()
 
 	USERNAME_FIELD = 'email'
-	REQUIRED_FIELDS = ['username']
+	REQUIRED_FIELDS = []
 
 	def __str__(self):
 			return self.email
--- users/urls.py
+++ users/urls.py
@@ -1,18 +1,29 @@
 from django.urls import path
-from .views import LoginView, LogoutView, SignupView, UserIndexView, AddUserView, RoleIndexView, AddRoleView, PermissionIndexView, AddPermissionView, UpdateRoleView, DeleteRoleView
+from .views import LoginView, LogoutView, SignupView, UserIndexView, AddUserView, RoleIndexView, AddRoleView, PermissionIndexView, AddPermissionView, UpdateRoleView, DeleteRoleView, UserDeleteView, UserUpdateView, UpdatePermissionView, DeletePermissionView, CustomAdminLoginView, CustomAdminLogoutView
 
 urlpatterns = [
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-    path('signup/', SignupView.as_view(), name='signup'),
+    path('admin_login/', CustomAdminLoginView.as_view(), name='admin_login'),
+    path('admin_logout/', CustomAdminLogoutView.as_view(), name='admin_logout'),
+    # path('admin/', admin_redirect, name='admin_redirect'),
+    
+    path('login_user/', LoginView.as_view(), name='login_user'),
+    path('logout_user/', LogoutView.as_view(), name='logout_user'),
+    path('signup_user/', SignupView.as_view(), name='signup_user'),
+
     path('user/index/', UserIndexView.as_view(), name='user_index'),
     path('user/add/', AddUserView.as_view(), name='add_user'),
+    path('user/update/<int:user_id>/', UserUpdateView.as_view(), name='update_user'),
+    path('user/delete/<int:user_id>/', UserDeleteView.as_view(), name='delete_user'),
+
     path('role/index', RoleIndexView.as_view(), name='role_index'),
     path('role/add', AddRoleView.as_view(), name='add_role'),
-    path('permission/add', AddPermissionView.as_view(), name='add_permission'),
-    path('permission/index', PermissionIndexView.as_view(), name='permission_index'),
     path('role/update/<int:role_id>/', UpdateRoleView.as_view(), name='update_role'),
     path('role/delete/<int:role_id>/', DeleteRoleView.as_view(), name='delete_role'),
+
+    path('permission/add', AddPermissionView.as_view(), name='add_permission'),
+    path('permission/index', PermissionIndexView.as_view(), name='permission_index'),
+    path('permission/delete/<int:perm_id>/', DeletePermissionView.as_view(), name='delete_permission'),
+    path('permission/update/<int:perm_id>/', UpdatePermissionView.as_view(), name='update_permission'),
 
     # Add more paths as needed for your application
 ]
