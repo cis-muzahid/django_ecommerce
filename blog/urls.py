3bea8b678e5371843bb179ab400690f2716d3087
AnjaliDubey2607 <anjali.d@cisinlabs.com>
2024-06-25 20:01:34 +0530

    added text editor for blog description

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,7 @@ from .views import *
 urlpatterns = [
     path('admin/blog/', BlogView.as_view(), name='admin_blog_view'),
     path('admin/blog/delete', BlogDeleteView.as_view(), name='delete_blog_view'),
+    path('user/blogs', UserBlogView.as_view(), name='user_blog_list'),
     path('user/blog/<int:pk>', UserBlogView.as_view(), name='user_blog'),
     path('admin/blog/<int:pk>', AdminBlogView.as_view(), name='admin_blog'),
 ]
